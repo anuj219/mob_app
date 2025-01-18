e40282df0e23eb75c658597332c0e4b2b97d60ed
pynuz <anujvmehta99@gmail.com>
2025-01-18 22:33:55 +0530

    Setup commit for ppt

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,    // this removes the banner that comes by default
+  ));
+}
+
